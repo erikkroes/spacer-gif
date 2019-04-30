a29329e61f86c71ed1c3658c664e6f724372f0fc
erikkroes <github@k4rp.nl>
2019-04-30 21:20:21 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# <spacer-gif\>
+
+`<spacer-gif>` is the most transparent way of adding space to your web page.
+
+- Proven technology
+- No confusing CSS needed
+- Anybody can do it
+- Comprehensive API
+- 0 Dependencies
+
+## 📦 Install it
+
+```
+npm i @erikkroes/spacer-gif
+```
+
+## 🛠️ Use it 
+
+```
+<script async type="module" src="/node_modules/@erikkroes/spacer-gif/spacer-gif.js"></script>
+
+<spacer-gif height="1" width="1"></spacer-gif>
+```
+
+## ✨⭐ But wait, there's more! ⭐✨
+
+Add color!
+
+```
+<spacer-gif height="1" width="1" bgcolor="#DD6633"></spacer-gif>
+```
---
+++ spacer-gif.js
@@ -0,0 +1,57 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+<style>
+  :host {
+    display: inline-block;       
+  }
+</style>
+`;
+
+class SpacerGif extends HTMLElement {
+  constructor() {
+    super();
+    this._shadowRoot = this.attachShadow({ 'mode': 'open' });
+    this._shadowRoot.appendChild(template.content.cloneNode(true));        
+  }
+
+  connectedCallback() {
+    if(!this.hasAttribute('height')) {
+      this.style.height = "1px";
+    }
+    else {
+      this.style.height = this._height;
+    }
+
+    if(!this.hasAttribute('width')) {
+      this.style.width = "1px";
+    }
+    else {
+      this.style.width = this._width;
+    }
+
+    if(this.hasAttribute('bgcolor')) {
+      this.style.backgroundColor = this._bgcolor;
+    }
+  }
+
+  static get observedAttributes() {
+    return ['height', 'width', 'bgcolor'];
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    switch(name){
+      case 'height':
+        this._height = newValue + "px";
+        break;
+      case 'width':
+        this._width = newValue + "px";
+        break;
+      case 'bgcolor':
+        this._bgcolor = newValue;
+        break;
+    }
+  }
+}
+
+window.customElements.define('spacer-gif', SpacerGif);
